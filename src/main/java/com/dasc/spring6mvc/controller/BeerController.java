66d13f2f22cd3d9ffc2efd416884cdb292f80d01
DMSANCHEZQ <davidsqr6@gmail.com>
2023-07-26 22:21:22 -0500

    feature: implement crud beer

--- src/main/java/com/dasc/spring6mvc/controller/BeerController.java
+++ src/main/java/com/dasc/spring6mvc/controller/BeerController.java
@@ -2,21 +2,60 @@ package com.dasc.spring6mvc.controller;
 
 import com.dasc.spring6mvc.model.Beer;
 import com.dasc.spring6mvc.service.BeerService;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api/v1/beers")
 public class BeerController {
 
   private final BeerService beerService;
 
-  public Beer getBeerById(UUID id) {
-    log.debug("Geet Beer by id in controller");
-    return beerService.getBeerById(id);
+  @DeleteMapping("{beerId}")
+  public ResponseEntity deleteById(@PathVariable UUID beerId) {
+    beerService.deleteBeerById(beerId);
+    return new ResponseEntity(HttpStatus.NO_CONTENT);
+  }
+
+  @PutMapping("{beerId}")
+  public ResponseEntity updateById(@PathVariable UUID beerId, @RequestBody Beer beer) {
+    beerService.updateBeerById(beerId, beer);
+    return new ResponseEntity(HttpStatus.NO_CONTENT);
+  }
+
+  @PostMapping
+  public ResponseEntity handlePost(@RequestBody Beer beer) {
+    Beer savedBeer = beerService.saveNewBeer(beer);
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Location", "/api/v1/beers/" + savedBeer.getId().toString());
+    return new ResponseEntity(headers, HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public List<Beer> listBeers() {
+    return beerService.listBeers();
+  }
+
+  @GetMapping("{beerId}")
+  public Beer getBeerById(@PathVariable UUID beerId) {
+    log.debug("Geet Beer by beerId in controller");
+    return beerService.getBeerById(beerId);
   }
 
 }
--- src/main/java/com/dasc/spring6mvc/service/BeerService.java
+++ src/main/java/com/dasc/spring6mvc/service/BeerService.java
@@ -1,10 +1,19 @@
 package com.dasc.spring6mvc.service;
 
 import com.dasc.spring6mvc.model.Beer;
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
 
+  List<Beer> listBeers();
+
   Beer getBeerById(UUID id);
+
+  Beer saveNewBeer(Beer beer);
+
+  void updateBeerById(UUID beerId, Beer beer);
+
+  void deleteBeerById(UUID beerId);
 
 }
