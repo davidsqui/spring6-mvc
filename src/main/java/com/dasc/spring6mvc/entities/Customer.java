a2711ea3dc0f3e75eec57084fdd1450f74e63350
DMSANCHEZQ <davidsqr6@gmail.com>
2023-08-06 13:31:20 -0500

    config: add flyway config

--- src/main/java/com/dasc/spring6mvc/entities/Customer.java
+++ src/main/java/com/dasc/spring6mvc/entities/Customer.java
@@ -1,15 +1,10 @@
 package com.dasc.spring6mvc.entities;
 
-import com.dasc.spring6mvc.model.BeerStyle;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -21,14 +16,13 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
-
-@Entity
-@Builder
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
-public class Beer {
+@NoArgsConstructor
+@Builder
+@Entity
+public class Customer {
 
   @Id
   @GeneratedValue(generator = "UUID")
@@ -36,26 +30,17 @@ public class Beer {
   @JdbcTypeCode(SqlTypes.CHAR)
   @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
   private UUID id;
+
+  private String name;
+
+  @Column(length = 100)
+  private String email;
+
   @Version
   private Integer version;
 
-  @NotNull
-  @NotBlank
-  @Size(max = 50)
-  @Column(length = 50)
-  private String beerName;
-
-  @NotNull
-  @Column(columnDefinition = "smallint")
-  private BeerStyle beerStyle;
-
-  @NotNull
-  @NotBlank
-  @Size(max = 255)
-  private String upc;
-  private Integer quantityOnHand;
-  private BigDecimal price;
   private LocalDateTime createdDate;
+
   private LocalDateTime updateDate;
 
 }
