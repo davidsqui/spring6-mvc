fc95608f0319f59590f9fd79c7871c367235374e
davidsqui <davidsqr6@gmail.com>
2023-12-14 23:55:14 -0500

    feat: add customerMapper

---
+++ src/main/java/com/dasc/spring6mvc/mappers/CustomerMapper.java
@@ -0,0 +1,13 @@
+package com.dasc.spring6mvc.mappers;
+
+import com.dasc.spring6mvc.entities.Customer;
+import com.dasc.spring6mvc.model.CustomerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface CustomerMapper {
+
+  Customer customerDtoToCustomer(CustomerDTO dto);
+
+  CustomerDTO customerToCustomerDto(Customer customer);
+}
